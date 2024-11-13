5a655e73125041834af1a075ccd8af985145ec9d
Taiocchi <luc.taio07@gmail.com>
2024-11-13 12:46:40 +0100

    Backend parte A

---
+++ Taiocchi_cassaforte/Taiocchi_cassaforte/Cassaforte.cs
@@ -0,0 +1,72 @@
+﻿namespace Taiocchi_cassaforte
+{
+    internal class Cassaforte
+    {
+        private string matricola;
+        private string produttore;
+        private string modello;
+        private string codiceSblocco12;
+        private string codiceUtente5;
+        private bool apertura = false; //Se true allora la cassaforte è aperta, altrimenti è chiusa
+        private bool stato = true; //Se true allora la cassaforte è sbloccata, altrimenti è bloccata
+
+        public void impostaCodiceUtente(string codice)
+        {
+            if (codice == null)
+                return;
+            codiceUtente5 = codice;
+        }
+
+        public void apri(string codice)
+        {
+            int n = 0;
+
+            while (n < 5)
+            {
+                if (codice == null)
+                    return;
+
+                n++;
+
+                if (codice == codiceUtente5)
+                {
+                    apertura = true;
+                    return;
+                }
+            }
+
+            stato = false;
+        }
+
+        public void chiudi()
+        {
+            apertura = false;
+        }
+
+        public void sblocca(string codice)
+        {
+            if (codice == null)
+                return;
+
+            if (codice == codiceSblocco12 && stato == false)
+                stato = true;
+
+        }
+
+        Cassaforte(string matricola,  string produttore, string modello, string codiceSblocco12)
+        {
+            if(this.matricola != null)
+                this.matricola = matricola;
+
+            if (this.produttore != null)
+                this.produttore = matricola;
+
+            if (this.modello != null)
+                this.modello = matricola;
+
+            if (this.codiceSblocco12 != null)
+                this.codiceSblocco12 = matricola;
+        }
+
+    }
+}
---
+++ Taiocchi_cassaforte/Taiocchi_cassaforte/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Taiocchi_cassaforte
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Cassaforte = new Label();
+            Stato = new Label();
+            Apertura = new Label();
+            Imposta = new Button();
+            SuspendLayout();
+            // 
+            // Cassaforte
+            // 
+            Cassaforte.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            Cassaforte.Location = new Point(605, 158);
+            Cassaforte.Name = "Cassaforte";
+            Cassaforte.Size = new Size(138, 43);
+            Cassaforte.TabIndex = 0;
+            Cassaforte.Text = "Cassaforte";
+            Cassaforte.Click += label1_Click;
+            // 
+            // Stato
+            // 
+            Stato.AutoSize = true;
+            Stato.Location = new Point(622, 211);
+            Stato.Name = "Stato";
+            Stato.Size = new Size(90, 15);
+            Stato.TabIndex = 1;
+            Stato.Text = "Stato: sbloccata";
+            // 
+            // Apertura
+            // 
+            Apertura.AutoSize = true;
+            Apertura.Location = new Point(622, 240);
+            Apertura.Name = "Apertura";
+            Apertura.Size = new Size(93, 15);
+            Apertura.TabIndex = 2;
+            Apertura.Text = "Apertura: chiusa";
+            // 
+            // Imposta
+            // 
+            Imposta.Location = new Point(63, 64);
+            Imposta.Name = "Imposta";
+            Imposta.Size = new Size(75, 23);
+            Imposta.TabIndex = 3;
+            Imposta.Text = "button1";
+            Imposta.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(Imposta);
+            Controls.Add(Apertura);
+            Controls.Add(Stato);
+            Controls.Add(Cassaforte);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label Cassaforte;
+        private Label Stato;
+        private Label Apertura;
+        private Button Imposta;
+    }
+}
